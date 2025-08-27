4f12e307fe96e9f91f2c40328bae44e68145b3e0
baampark <baam.park.95@gmail.com>
2025-08-27 00:31:40 -0400

    add cors policy

--- labelmv-backend/app.py
+++ labelmv-backend/app.py
@@ -1,9 +1,10 @@
 
-
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import os
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/videos', methods=['GET'])
 def get_videos():
